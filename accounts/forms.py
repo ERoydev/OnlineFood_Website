6abea975d8b545d0109db798fdb2afcaf5e50d7b
Emil Roydev <e.roydev@gmail.com>
2023-11-23 19:25:29 -0800

    prepared files for google API location

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,6 +23,7 @@ class UserForm(forms.ModelForm):
       )
     
 class UserProfileForm(forms.ModelForm):
+  address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Start typing...', 'required': 'required'}))
   profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
   cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[allow_only_images_validator])
 
@@ -30,4 +31,4 @@ class UserProfileForm(forms.ModelForm):
   longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly': "readonly"}))
   class Meta:
     model = UserProfile
-    fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+    fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
